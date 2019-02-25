e169bbc19d9d9203a847a5a895032bb4a7ab450f
sweetmilkys <sweetmilky.dev@gmail.com>
2019-02-26 00:07:40 +0900

    Express Core: Routing

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-// Find express folder in my node_modules folder and
 import express from "express";
 import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-
+import { userRouter } from "./router";
 const app = express();
-
-const PORT = 4000;
-
-const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
 
 const handleHome = (req, res) => res.send("hello world");
 
 const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(cookieParser()); // cookies middleware
-app.use(bodyParser().json()); // body middleware
+app.use(bodyParser.json()); // body middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet()); // secure middleware
 app.use(morgan("dev")); // logger middleware
 
-app.get("/", middleware, handleHome); // respond with "hello world" when a GET request is made to the homepage
+app.get("/", handleHome); // respond with "hello world" when a GET request is made to the homepage
 
 app.get("/profile", handleProfile);
 
-app.listen(PORT, handleListening); // set port
+app.use("/user", userRouter);
+
+export default app;
