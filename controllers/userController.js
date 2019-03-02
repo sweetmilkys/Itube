09aa37710f6baf1e4a4d01927b431c0793ad3b4f
sweetmilkys <sweetmilky.dev@gmail.com>
2019-03-02 12:23:33 +0900

    Join Controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,31 @@
-export const join = (req, res) => res.render("join", { pageTitle: "Join" });
+import routers from "../routers";
+
+export const getJoin = (req, res) => {
+  res.render("join", { pageTitle: "Join" });
+};
+
+export const postJoin = (req, res) => {
+  const {
+    body: { name, email, password, password2 }
+  } = req;
+  if (password !== password2) {
+    res.status(400);
+  } else {
+    res.redirect(routers.home);
+  }
+  res.render("join", { pageTitle: "Join" });
+};
+
 export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+
 export const logout = (req, res) =>
   res.render("logout", { pageTitle: "Log Out" });
+
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
+
 export const editProfile = (req, res) =>
   res.render("editProfile", { pageTitle: "Edit Profile" });
+
 export const changePassword = (req, res) =>
   res.render("changePassword", { pageTitle: "Change Password" });
--- routers/globalRouter.js
+++ routers/globalRouter.js
@@ -1,14 +1,20 @@
 import express from "express";
 import routers from "../routers";
 import { home, search } from "../controllers/videoController";
-import { join, login, logout } from "../controllers/userController";
+import {
+  getJoin,
+  postJoin,
+  login,
+  logout
+} from "../controllers/userController";
 
 const globalRouter = express.Router();
 
 globalRouter.get(routers.home, home);
-globalRouter.get(routers.join, join);
+globalRouter.get(routers.search, search);
+globalRouter.get(routers.join, getJoin);
+globalRouter.post(routers.join, postJoin);
 globalRouter.get(routers.login, login);
 globalRouter.get(routers.logout, logout);
-globalRouter.get(routers.search, search);
 
 export default globalRouter;
