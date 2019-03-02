15594a32c4aa4fa58c7dafdad4102f7033185bfe
sweetmilkys <sweetmilky.dev@gmail.com>
2019-03-02 17:16:07 +0900

    Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,54 +1,17 @@
-export const videos = [
+import mongoose from "mongoose";
+
+mongoose.connect(
+  "mongodb://localhost:27017/wetube",
   {
-    id: 325393,
-    title: "Video awesome",
-    description: "This is something I love",
-    views: "24",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "danah",
-      email: "sweetmilky.dev@gmail.com"
-    }
-  },
-  {
-    id: 1212121,
-    title: "Video super",
-    description: "This is something I love",
-    views: "24",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "danah",
-      email: "sweetmilky.dev@gmail.com"
-    }
-  },
-  {
-    id: 55555,
-    title: "Video nice",
-    description: "This is something I love",
-    views: "24",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "danah",
-      email: "sweetmilky.dev@gmail.com"
-    }
-  },
-  {
-    id: 11111,
-    title: "Video perfect",
-    description: "This is something I love",
-    views: "24",
-    videoFile:
-      "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4",
-    creator: {
-      id: 121212,
-      name: "danah",
-      email: "sweetmilky.dev@gmail.com"
-    }
+    useNewUrlParser: true,
+    useFindAndModify: false
   }
-];
+);
+
+const db = mongoose.connection;
+
+const handleOpen = () => console.log(`✅  Connected DB`);
+const handleError = () => console.log(`❌  Error on DB Connection:${error}`);
+
+db.once("open", handleOpen);
+db.on("error", handleError);
